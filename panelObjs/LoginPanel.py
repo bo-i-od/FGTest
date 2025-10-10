b1805ee6d1c1523f2232b4faf835a9f5fac6f672
lkl-lu <18776402026@163.com>
2025-10-10 16:59:41 +0800

    登陆界面操作方法

---
+++ panelObjs/LoginPanel.py
@@ -0,0 +1,46 @@
+from common.basePage import BasePage
+from configs.elementsData import ElementsData
+
+class LoginPanel(BasePage):
+    def is_panel_active(self):
+        return self.exist(element_data=ElementsData.LoginPanel.LoginPanel)
+
+    def wait_for_panel_appear(self):
+        self.wait_for_appear(element_data=ElementsData.LoginPanel.LoginPanel, ignore_set={"LoginPanel"})
+
+    def is_btn_login_active(self):
+        object_id_list = self.get_object_id_list(element_data_list=[ElementsData.LoginPanel.btn_login])
+        if object_id_list[0] or object_id_list[1]:
+            return True
+        return False
+
+    def wait_for_btn_login(self):
+        self.wait_for_appear(element_data_list=[ElementsData.LoginPanel.btn_login], timeout=20)
+
+    def click_btn_login(self):
+        self.click_element(element_data=ElementsData.LoginPanel.btn_login)
+        # btn_login_element = ElementsData.LoginPanel.btn_login
+        # self.click_until_disappear(element_data=btn_login_element, interval=2, ignore_set={"LoginPanel"})
+
+    def is_InputField_UserName_active(self):
+        return self.exist(element_data=ElementsData.LoginPanel.InputField_UserName)
+
+    def set_login_name(self, login_name):
+        self.set_text(element_data=ElementsData.LoginPanel.InputField_UserName, text=login_name)
+
+    def get_login_name(self):
+        login_name = self.get_text(element_data=ElementsData.LoginPanel.InputField_UserName)
+        return login_name
+
+    def set_server(self, index):
+        self.set_dropdown_value(element_data=ElementsData.LoginPanel.Dropdown, index=index)
+
+    operation_pool = [
+        {"element_data": ElementsData.LoginPanel.btn_login, "func": click_btn_login, "weight": 1},
+    ]
+
+
+if __name__ == "__main__":
+    bp = BasePage()
+
+    bp.connect_close()
